a97600bd5c4c0368b06b35cee4fe24a285723d1c
Pratham-sharma07 <102985188+Pratham-sharma07@users.noreply.github.com>
2022-05-12 23:39:35 +0530

    Add files via upload

---
+++ ass3 output.py
@@ -0,0 +1,76 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======================== RESTART: C:/Python 310/ass3.py ========================
+Enter any number: 2
+Binary equivalent of  2 is:  0b10 
+
+Welcome to this interactive python calculator
+Enter the expressions by using only these operators (+,-,,/,*,%,//):8*8
+The result is  64
+
+
+Result of part (a) is:  35
+
+
+Choose the value of n
+The value of n taken is: 2
+The result of part (b) is:  1.0
+
+
+Choose the value of r
+The value of radius taken in: 6
+The result of part (c) is:  452.3893421169302
+Enter the value of r: 4
+Enter the value of angle a: 30
+Enter the value of angle b: 60
+The result of part (d) is:  0.2334060490582819
+
+
+enter the value of x1:4
+enter the value of x2:5
+enter the value of y1:6
+enter the value of y2:7
+The result of part (e) is:  (y2-y1)/(x2-x1) 1.0
+
+
+In the range(5): 
+0
+1
+2
+3
+4
+In the range(3, 10): 
+3
+4
+5
+6
+7
+8
+9
+
+
+In the range(4, 13, 3): 
+4
+7
+10
+
+
+In the range(15 ,5, -2): 
+15
+13
+11
+9
+7
+
+
+In the range(5, 3): 
+
+
+This program computes molecular weight of
+carbohydrates, for this we need to enter number of
+atoms of hydrogen, carbon and oxygen.
+Enter the number of hydrogen atoms: 6
+Enter the number of carbon atoms:6
+Enter the number of oxygen atoms:2
+The weight of carbohydrate:  110.11064
